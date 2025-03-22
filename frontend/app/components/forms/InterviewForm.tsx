4413171ad2e3ea2505ea6e6b7c7b20f34496699e
michaelwaves <michaelyu713705@gmail.com>
2025-03-22 19:21:04 -0400

    commit

--- frontend/app/components/forms/InterviewForm.tsx
+++ frontend/app/components/forms/InterviewForm.tsx
@@ -1,19 +1,23 @@
 "use client"
 
 import { Button } from "@/components/ui/button";
+import { updateOne } from "@/lib/postgres/tables/single";
 import { createConversation, endConversation, getTranscript } from "@/lib/tavus/tavus";
 import { useState } from "react";
 import { toast } from "sonner";
 
 function InterviewForm({ referenceId, id }: { referenceId: string, id: string }) {
 
-    const [url, setUrl] = useState<string | null>(null)
+    const [url, setUrl] = useState<string | null>(null);
     const [transcript, setTranscript] = useState<any>();
+    const [conversationId, setConversationId] = useState<string | null>();
     const handleStartConversation = async () => {
         try {
             const res = await createConversation()
             window.open(res, "_blank", "noopener,noreferrer"); // Open in new tab
             setUrl(res)
+            const convId = res.split("/").at(-1)
+            setConversationId(convId)
             toast("Sucessfully created Conversation")
         } catch (e) {
             console.error(e)
@@ -26,7 +30,7 @@ function InterviewForm({ referenceId, id }: { referenceId: string, id: string })
             if (!url) {
                 return
             }
-            const conversationId = url.split("/").at(-1)
+
             const res = await endConversation(conversationId ?? "")
             toast("Sucessfully ending Conversation")
         } catch (e) {
@@ -36,18 +40,27 @@ function InterviewForm({ referenceId, id }: { referenceId: string, id: string })
     }
 
     const handleSubmitConversation = async () => {
-        const res = await getTranscript("c5cfe8e3cffa")
+        const res = await getTranscript(conversationId ?? "")
         console.log(res)
         const index = res.events.findIndex((event: any) => event.event_type === "application.transcription_ready");
 
+        let myTranscript;
         if (index !== -1) {
-            const myTranscript = res.events[index].properties.transcript;
+            myTranscript = res.events[index].properties.transcript;
             console.log(myTranscript);
             setTranscript(myTranscript);
         } else {
             console.log("No transcription event found");
             setTranscript("No transcription events found");
         }
+
+        const updateFriendRes = await updateOne("friends", referenceId, {
+            status: "interview completed",
+            tavus_transcript: myTranscript,
+            tavus_conversation_id: conversationId
+        })
+
+        console.log(updateFriendRes)
 
 
     }
