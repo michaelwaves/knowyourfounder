08362ef9357f6cf9b978a67b1f38c4a6ec6c9d54
michaelwaves <michaelyu713705@gmail.com>
2025-03-22 19:07:03 -0400

    commit

--- frontend/lib/aws/ses/sendEmailVideoInterview.tsx
+++ frontend/lib/aws/ses/sendEmailVideoInterview.tsx
@@ -6,7 +6,7 @@ const ses = new SES({ region: process.env.AWS_SES_REGION });
 
 const logoLink = "https://substackcdn.com/image/fetch/f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2F0588111d-fd5e-4f52-b979-a05fdbaf8ee5_3496x2784.png"
 
-export async function sendAWSEmail(toEmail: string, subject: string) {
+export async function sendVideoInterviewEmail(toEmail: string, subject: string, link: string) {
     const emailHtml = `    <div style="background-color: #9bb4dd; padding: 20px; font-family: Arial, sans-serif; color: #333;">
         <div
             style="max-width: 600px; margin: 0 auto; border-radius: 8px; background-color: #ffffff; padding: 20px; box-shadow: 0 4px 8px rgba(0,0,0,0.1);">
@@ -15,11 +15,11 @@ export async function sendAWSEmail(toEmail: string, subject: string) {
             <h2 style="color: #333; font-size: 24px; font-weight: bold;">Know Your Founder</h2>
             <p style="font-size: 16px; line-height: 1.6; color: #666;">Hello,</p>
             <p style="font-size: 16px; line-height: 1.6; color: #666;">
-                I hope you're doing well! I'm conducting a reference check for [PERSON X], who worked with you at [COMPANY Y]. Could you kindly confirm if you collaborated with him? I'd also appreciate your feedback on his strengths, growth areas, work style, and any challenges you encountered. Thanks!
+                I hope you're doing well! I'm conducting a reference check for Alex Chan, who worked with you at Shopify. Could you kindly confirm if you collaborated with him? I'd also appreciate your feedback on his strengths, growth areas, work style, and any challenges you encountered. Thanks!
             </p>
             <p style="font-size: 16px; line-height: 1.6; color: #666;">You can now provide reference check by clicking the link below:
             </p>
-            <a href="https://app.quantoflow.com/" target="_blank"
+            <a href="${link}" target="_blank"
                 style="display: inline-block; margin-top: 15px; padding: 10px 20px; font-size: 16px; color: #ffffff; background-color: #4d56a1; border-radius: 5px; text-decoration: none;">Login
                 Here</a>
             <p style="font-size: 12px; line-height: 1.4; color: #999; margin-top: 20px;">If you have any questions, feel
